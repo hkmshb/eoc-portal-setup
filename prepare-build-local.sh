a6c9615c3556759e80fcd9fe20531db7a48f85f8
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-11-13 14:44:34 +0100

    update prepare-build-local.sh with CKAN_VERSION re-order come commands

--- prepare-build-local.sh
+++ prepare-build-local.sh
@@ -19,6 +19,7 @@ export SMTP_SERVER=${SMTP_SERVER}
 export SMTP_USER=${SMTP_USER}
 export SMTP_PASSWORD=${SMTP_PASSWORD}
 
+export CKAN_VERSION=${CKAN_VERSION}
 export CKAN_SITE_URL="http://${CKAN_DB_HOST}"
 export CKAN_DB_USER="ckan"
 export CKAN_DB_HOST="db"
@@ -34,15 +35,13 @@ export ENV="local"
 # export GOOGLE_EMAIL="${GOOGLE_EMAIL}"
 # export GOOGLE_PASSWORD="${GOOGLE_PASSWORD}"
 
-# gather2
+## gather2
 export GATHER2_API_KEY=${GATHER2_API_KEY}
 export GATHER2_RESPONSES_URL=${GATHER2_RESPONSES_URL}
 
+## elk
 export ELK_VERSION=${ELK_VERSION}
 
-
-cat docker-compose.yml.template | envsubst > docker-compose.yml
-cat ./ckan_setup/conf/postgres/ckan_init.sql.template | envsubst > ./ckan_setup/conf/postgres/ckan_init.sql
 
 if [ -d ckan_setup ]; then
   echo ckan_setup already exists
@@ -50,10 +49,12 @@ else
   git clone --branch master --depth 1 https://github.com/ehealthAfrica/ckan_setup.git ckan_setup
 
   # we need more than the depth of 1 to be able to checkout to the 2.7 tag we use in build.sh
-  git clone --branch '${CKAN_VERSION}' --single-branch --depth 1 https://github.com/ckan/ckan.git ./ckan_setup/ckan
+  git clone --branch ${CKAN_VERSION} --single-branch --depth 1 https://github.com/ckan/ckan.git ./ckan_setup/ckan
   git clone --branch master https://github.com/ckan/datapusher.git ./ckan_setup/datapusher
 fi
 
+cat docker-compose.yml.template | envsubst > docker-compose.yml
+cat ./ckan_setup/conf/postgres/ckan_init.sql.template | envsubst > ./ckan_setup/conf/postgres/ckan_init.sql
 
 trap : 0
 echo >&2 '*** DONE ***'
